8f8f3aae5fcfa8fe998edcb431b323b0a4a2cfc2
varaprasad <varaprasad.urumadla@ratnaglobaltech.com>
2023-08-17 19:44:06 +0530

    profile changes

--- src/AllRoutes/index.tsx
+++ src/AllRoutes/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import HomePage from '../components/views/HomePage';
-import SignUp from '../components/views/SignUp';
+import Profile from '../components/views/Profile/Profile';
+import SignUp from '../components/views/Signup';
 
 const AllRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/registration" element={<SignUp />} />
+            <Route path="/profile" element={<Profile />} />
         </Routes>
     )
 }
