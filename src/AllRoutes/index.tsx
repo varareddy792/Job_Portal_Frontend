2fce7adafb9552912228e299ee1e885123399bd3
Dibyalochan <dibyalochanparida@gmail.com>
2023-08-17 19:10:31 +0530

    added homepage

--- src/AllRoutes/index.tsx
+++ src/AllRoutes/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
-import HomePage from '../components/views/HomePage';
+import LandingPage from '../components/views/LandingPage';
 import SignUp from '../components/views/SignUp';
+import HomePage from '../components/views/HomePage';
 
 const AllRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<LandingPage />} />
             <Route path="/registration" element={<SignUp />} />
+            <Route path="/homePage" element={<HomePage />} />
         </Routes>
     )
 }
